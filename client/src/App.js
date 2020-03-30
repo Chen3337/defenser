9f072c6eb5168cfcdff7484029df91ca49d47d61
zhen yong chen <dannychen111@yahoo.com>
2020-03-29 23:27:15 -0500

    test if able to be langdscape mode

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    isLandscape: window.screen.orientation.type
+  }
+  componentDidMount() {
+    window.screen.orientation.addEventListener('change', () => {
+      this.setState({
+        isLandscape: window.screen.orientation.type
+      })
+    });
+  }
+  render() {
+
+    return (
+      <div>
+        {this.state.isLandscape.charAt(0) === "l"
+        ? <h2>{this.state.isLandscape}</h2>
+        : <h2>{this.state.isLandscape} not Landscape</h2>
+      }
+      </div>
+    )
+  }
 }
 
 export default App;

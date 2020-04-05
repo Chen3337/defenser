dda8c910aaba61ef239f69509ae1cd104147e359
zhen yong chen <dannychen111@yahoo.com>
2020-04-04 22:24:01 -0500

    landscape detector

--- client/src/App.js
+++ client/src/App.js
@@ -2,31 +2,33 @@ import React, { Component } from 'react';
 import Router from './components/router';
 class App extends Component {
   state = {
-    isLandscape: 90
+    isLandscape: window.orientation
   }
   componentDidMount() {
-      if ((window.innerWidth - 100) > window.innerHeight) {
-        this.setState({
-          isLandscape: 90
-        });
-      }
       window.addEventListener("resize", () => {
-        if ((window.innerWidth - 100) > window.innerHeight) {
-          this.setState({
-            isLandscape: 90
-          });
-        }
-        else {
-          this.setState({
-            isLandscape: 0
-          })
-        }
+        this.checkIfLandscape;
       })
+      window.addEventListener("orientationchange", () => {
+        this.checkIfLandscape;
+      });
+      this.checkIfLandscape;
+}
+checkIfLandscape = () =>{
+  if ((window.innerWidth - 100) > window.innerHeight) {
+    this.setState({
+      isLandscape: 90
+    });
   }
+  else {
+    this.setState({
+      isLandscape: 0
+    })
+  }
+}
   render() {
     return (
       <div>
-        {Math.abs(this.state.isLandscape) === 90
+        {this.state.isLandscape === 90
           ? <Router />
           : <h2>rotate the screen or make the width of the screen larger than the height to begain</h2>
         }

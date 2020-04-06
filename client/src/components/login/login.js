1d006ba92a449f04161785da7c503d196163d1f8
zhen yong chen <dannychen111@yahoo.com>
2020-04-06 12:38:32 -0500

    login page layout

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import MainImage from '../../image/main.jpg';
 function Login() {
-    console.log(window.innerWidth);
     return (
         <div style={{backgroundImage: `url(${MainImage})`, width:"100%", height:"100vh", backgroundPosition: 'center', backgroundRepeat:'no-repeat', backgroundSize:'cover'}}>
-            <div style={{textAlign:"center", padding:'20px', position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%, -50%)', width: '40%', height: "80vh", backgroundColor: "#ebde34", borderRadius: '25px', opacity:'.9' }}>
+            <div style={{textAlign:"center", padding:'20px', position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%, -50%)', width: '35%', height: "60vh", backgroundColor: "#ebde34", borderRadius: '25px', opacity:'.9' }}>
                 <h2>DEFENSER</h2>
                 username:
                 <input></input>
@@ -12,6 +11,12 @@ function Login() {
                 <br></br>
                 password: 
                 <input></input>
+                <br></br>
+                <br></br>
+                <button style={{width:'60%', backgroundColor:'green', color:'white', padding:'5px'}}>login</button>
+                <br></br>
+                <br></br>
+                <a href="/signup"> [create account here]</a>
             </div>
         </div>
     )

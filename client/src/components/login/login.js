9e6eaf8eae98664d5aca8e94fb3608bf0a6e91e2
zhen yong chen <dannychen111@yahoo.com>
2020-04-06 11:33:51 -0500

    see location on screen

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -4,8 +4,14 @@ function Login() {
     console.log(window.innerWidth);
     return (
         <div style={{backgroundImage: `url(${MainImage})`, width:"100%", height:"100vh", backgroundPosition: 'center', backgroundRepeat:'no-repeat', backgroundSize:'cover'}}>
-            <div style={{ position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%, -50%)', width: '40%', height: "80vh", backgroundColor: "#ebde34", borderRadius: '25px' }}>
-
+            <div style={{textAlign:"center", padding:'20px', position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%, -50%)', width: '40%', height: "80vh", backgroundColor: "#ebde34", borderRadius: '25px', opacity:'.9' }}>
+                <h2>DEFENSER</h2>
+                username:
+                <input></input>
+                <br></br>
+                <br></br>
+                password: 
+                <input></input>
             </div>
         </div>
     )

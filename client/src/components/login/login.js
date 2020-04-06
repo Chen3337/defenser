f3f3b92f2a40cda9ee76d28d1501d423cd72c08f
zhen yong chen <dannychen111@yahoo.com>
2020-04-05 23:34:24 -0500

    login page setting up testing

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import MainImage from '../../image/main.jpg';
 function Login() {
+    console.log(window.innerWidth);
     return (
-        <div style={{
-            position: 'absolute', left: '50%', top: '50%',
-            transform: 'translate(-50%, -50%)'
-        }}>
-            login page here
+        <div style={{backgroundImage: `url(${MainImage})`, width:"100%", height:"100vh", backgroundPosition: 'center', backgroundRepeat:'no-repeat', backgroundSize:'cover'}}>
+            <div style={{ position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%, -50%)', width: '40%', height: "80vh", backgroundColor: "#ebde34", borderRadius: '25px' }}>
+
+            </div>
         </div>
     )
 }

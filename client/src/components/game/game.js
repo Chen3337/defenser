4842cea4b0cb57e3cb2ec1d9e6a30fdc2030b787
zhen yong chen <dannychen111@yahoo.com>
2020-04-08 16:48:24 -0500

    first sprite testing

--- client/src/components/game/game.js
+++ client/src/components/game/game.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import MainImage from '../../image/game.jpg';
 import GroundImage from '../../image/ground.PNG';
-import Image from '../../image/monstertwo.png';
+import Image from '../../image/monsterone.png';
+import Monsterone from './monsterone';
 class Game extends Component {
     constructor(props) {
         super(props);
@@ -11,20 +12,35 @@ class Game extends Component {
     state = {
         screenWidth: window.innerWidth,
         screenHeight: window.innerHeight,
+        monsteroneImage: null,
+        monsterone: new Monsterone(),
+        context: null,
     }
 
     componentDidMount() {
+        window.onload = () => {
+            requestAnimationFrame(() => { this.update() });
+        }
+    }
+    update = () => {
+        this.state.monsterone.render(this.state);
+        requestAnimationFrame(() => { this.update() });
     }
     testing = () => {
         const context = this.canvas.current.getContext('2d');
-        context.scale(-1, 1);
-        context.drawImage(this.image.current, 0, 0, 130, 180, -100, (this.state.screenHeight /10 * 9) - 50, 50, 50);
+        this.setState({
+            monsteroneImage: this.image.current,
+            context: context,
+            
+        })
+
+
     }
     render() {
         return (
             <div>
                 <img style={{ display: 'none' }} ref={this.image} src={Image} alt="sprite" onLoad={() => { this.testing() }} />
-                <img style={{ width: "100%", height: '10vh', position: "fixed", bottom: "0" }} src={GroundImage} alt="ground" />
+                <img style={{ width: "100%", height: '20vh', position: "fixed", bottom: "0" }} src={GroundImage} alt="ground" />
                 <div style={{ backgroundImage: `url(${MainImage})`, width: "100%", height: "100vh", backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
 
                     <canvas ref={this.canvas}
---
+++ client/src/components/game/monsterone.js
@@ -0,0 +1,25 @@
+class Monsterone {
+    constructor() {
+        this.ScreenX = window.innerWidth;
+        this.ScreenY = window.innerHeight;
+        this.x = -200;
+        this.y = this.ScreenY * 0.8;
+        this.sprite = {
+            startX : 0,
+            startY : 0,
+            width : 130,
+            height : 180,
+        }
+        this.sizeX = this.ScreenX * 0.1;
+        this.sizeY = this.ScreenY * 0.2;
+    }
+    
+    render(state) {
+        const context = state.context;
+        context.scale(-1, 1);
+        // drawimage(image, image startx, starty, widthsize, heightsize
+        // , canvas x location, canvas y location, canvas image size x, canvas image size y)
+        context.drawImage(state.monsteroneImage, this.sprite.startX, this.sprite.startY, this.sprite.width, this.sprite.height, this.x, (this.y - this.sizeY), this.sizeX, this.sizeY);
+    }
+}
+export default Monsterone

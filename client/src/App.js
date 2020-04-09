cdb6edd97b3b1b9545636f748da76314ba9ef14b
zhen yong chen <dannychen111@yahoo.com>
2020-04-08 23:07:52 -0500

    window.load problem fixed

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
   }
   componentDidMount() {
     window.addEventListener("resize", () => {
+      window.location.reload();
       this.checkIfLandscape();
     })
     window.addEventListener("orientationchange", () => {
